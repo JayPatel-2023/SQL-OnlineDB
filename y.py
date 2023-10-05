8f010e8992cf9beb7286d46185280645c143619b
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-10-05 21:09:10 +0530

    update

--- y.py
+++ y.py
@@ -13,7 +13,7 @@ if response.status_code == 200:
     print("Output from PHP:", response.text)
 else:
     print(f"Error: {response.status_code}")
-
+'''
 import requests
 
 # Replace this with the URL of your modified PHP file on GitHub
@@ -32,8 +32,8 @@ if response.status_code == 200:
     print("Result from PHP function:", result)
 else:
     print(f"Error: {response.status_code}")
-'''
 
+'''
 import requests
 import json
 
@@ -58,3 +58,4 @@ if response.status_code == 200:
         print("Unexpected JSON format")
 else:
     print(f"Error: {response.status_code}")
+'''
